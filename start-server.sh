8ac0a30de8c095a8c7b75afea543f8fefc76b595
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-30 08:49:56 +0200

    Update start-server.sh
    removed unnecessary code (the path) from the file

--- start-server.sh
+++ start-server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Navigate to the Laravel project directory (Modify this if the path is different)
-cd /mnt/c/Users/kneno/PhpstormProjects/RealEstate1/
-
 # Start the Sail containers
 ./vendor/bin/sail up -d
 
