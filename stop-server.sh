aa970a180428df9c4cef9c5460288fae04f7e9a1
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-30 08:50:21 +0200

    Update stop-server.sh
    removed unnecessary code (the path) from the file

--- stop-server.sh
+++ stop-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Navigate to the Laravel project directory (Modify this if the path is different)
-cd /mnt/c/Users/kneno/PhpstormProjects/RealEstate1/
 
 # Stop the Sail containers
 ./vendor/bin/sail down
