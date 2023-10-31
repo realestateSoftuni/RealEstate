efcac4fef84af1ab8e12585917234b121cbb912c
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-31 09:11:27 +0200

    Update start-server.sh
    Added a conditional check to ensure the server starts up correctly, and to provide feedback to the user in case of failure.

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,10 @@
 # Start the Sail containers
 sh ./vendor/bin/sail up -d
 
+if [[ $? -ne 0 ]]; then
+    echo "Server failed to start."
+    exit 1
+fi
+
 # Print a message
 echo "Server started successfully. Visit http://localhost:$(grep APP_PORT .env | cut -d '=' -f2) to access the application."
