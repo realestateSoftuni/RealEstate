a7eed54c530f63f4f6ecb4fe48c869623bfc6081
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-31 07:45:51 +0200

    Update start-server.sh
    added 'sh': Starts a new interactive shell session or can be used to execute a script.

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the Sail containers
-./vendor/bin/sail up -d
+sh ./vendor/bin/sail up -d
 
 # Print a message
 echo "Server started successfully. Visit http://localhost:$(grep APP_PORT .env | cut -d '=' -f2) to access the application."
