417cdfd6a5106e9e9b522bf8ced578a6c9616fc3
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-11-01 10:40:55 +0200

    Update start-server.sh
    edited script for starting docker for linux environment

--- start-server.sh
+++ start-server.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+# Check for Windows OS (this might detect WSL environments as well)
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+    echo "Detected Windows OS. Please ensure Docker Desktop is running and then proceed."
+    # Optionally, you can exit or just continue with the rest of your script
+    # exit 1
+else
+    # Check if Docker is running on Linux
+    docker info > /dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo "Docker is not running. Attempting to start Docker..."
+
+        # Start Docker using systemd
+        sudo systemctl start docker
+
+        # Wait a bit to ensure Docker starts up
+        sleep 5
+
+        # Check again if Docker is running after the attempt to start
+        docker info > /dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+            echo "Failed to start Docker. Please start it manually or check its status."
+            exit 1
+        fi
+    fi
+fi
+
 # Start the Sail containers
 ./vendor/bin/sail up -d
-
 
 if [ $? -ne 0 ]; then
     echo "Server failed to start."
