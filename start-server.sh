0ce31fd90a33455d7633baa4c7679d69f271bc59
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-11-01 10:13:31 +0200

    Update start-server.sh
    fixed problem with error displaying in terminal (./start-server.sh: 7: [[: not found")

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Start the Sail containers
-sh ./vendor/bin/sail up -d
+./vendor/bin/sail up -d
 
-if [[ $? -ne 0 ]]; then
+
+if [ $? -ne 0 ]; then
     echo "Server failed to start."
     exit 1
+else
+    # Print a message only if sail command was successful
+    echo "Server started successfully. Visit http://localhost:$(grep APP_PORT .env | cut -d '=' -f2) to access the application."
 fi
-
-# Print a message
-echo "Server started successfully. Visit http://localhost:$(grep APP_PORT .env | cut -d '=' -f2) to access the application."
