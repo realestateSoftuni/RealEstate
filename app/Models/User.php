e4dc6372ec2bc947861891622b2dcb2acfc687b6
nenovKrasimir <k.nenov09@gmail.com>
2023-11-03 00:17:24 +0200

    -imported Agent class

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,15 @@
 
 namespace App\Models;
 
+use App\Models\Agent;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasFactory;
 
     const ROLE_CUSTOMER = 'customer';
     const ROLE_AGENT = 'agent';
