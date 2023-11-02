ec2f3d429f684e712edbcdcb049b4df75a3b8b2a
nenovKrasimir <k.nenov09@gmail.com>
2023-11-03 00:16:58 +0200

    -Created agent migration -Created agent model -Created agent factory

---
+++ app/Models/Agent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+
+class Agent extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'years_of_experience',
+        'total_properties_sold',
+        'average_rating',
+        'update_count',
+    ];
+
+    /**
+     * Get the user that the agent profile belongs to.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the properties associated with the agent.
+     */
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
+
+    protected static function booted()
+    {
+        static::saving(function ($agent) {
+            // Increment 'update_count' when any field other than 'update_count' is being updated
+            if ($agent->isDirty() && !$agent->isDirty('update_count')) {
+                $agent->update_count += 1;
+            }
+        });
+    }
+}
